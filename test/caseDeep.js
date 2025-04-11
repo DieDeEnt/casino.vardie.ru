d372b8ed05ea4971a728ae2c1e172abe375f620b
die <diedeent@gmail.com>
2025-04-11 20:32:42 +0300

    df

--- test/caseDeep.js
+++ test/caseDeep.js
@@ -154,10 +154,7 @@ async function animateRoulette(targetItem) {
     const uniqueItems = [...new Set(items.map(i => i.id))];
     const targetIndex = uniqueItems.indexOf(targetItem.id);
 
-    const targetPosition = 
-    (targetIndex * itemWidth) - 
-    (containerWidth / 2) + 
-    (itemWidth / 2);
+    const targetPosition = (targetIndex * itemWidth) - (containerWidth / 2) + (itemWidth / 2);
 
     track.style.transition = `transform ${Math.min(5000, items.length * 50)}ms cubic-bezier(0.25, 0.1, 0.25, 1)`;
     track.style.transform = `translateX(${-targetPosition}px)`;
